18425dbf965aac5803ae6e58221f2dbd40de62d8
Vadym Vynnychuk <vadimvinnicuk4@gmail.com>
2020-03-19 19:09:57 +0200

    master

---
+++ js/learn1.js
@@ -0,0 +1,24 @@
+let name = prompt ("Як тебе звати?");
+alert( "Камарджоба Братан " + name );
+
+
+let year = prompt("рік народження ?");
+const nowYear = 2020;
+let yourYear = nowYear - year;
+alert(`Вам ${yourYear} років`);
+
+let squareSideSize = prompt('який розмір сторони квадрату?');
+let squarePerimeter = (squareSideSize*4);
+alert(`Ваш периметр квадрата ${squarePerimeter}`);
+
+
+
+let circleRadius = prompt('який радіус кола?');
+let CircleArea = ( Math.PI * (circleRadius * circleRadius));
+alert(`Ваша площа кола? ${CircleArea}`);
+
+
+
+
+
+         
